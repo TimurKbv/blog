aa4a89253c4cd95fbdf4723ed01c232f363e4c41
Timur Kbv <timur.kokumbaev@gmail.com>
2024-02-16 19:19:00 +0100

    underline for nav-links

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='w-full h-[10vh] flex justify-evenly items-center'>
-        <Link href="/" >Home</Link>
-        <Link href="/blog" >Blog</Link>
-        <Link href="/about" >About</Link>
+        <Link href="/" className='hover:underline' >Home</Link>
+        <Link href="/blog" className='hover:underline'>Blog</Link>
+        <Link href="/about" className='hover:underline'>About</Link>
     </header>
   )
 }
